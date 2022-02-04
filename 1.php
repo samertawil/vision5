5b681c63311b6e3181058e6a0a480ad600b50237
samer taweel <samer@labor.gaza>
2022-02-04 16:59:55 +0200

    first copy

---
+++ 1.php
@@ -0,0 +1,266 @@
+<?php
+
+// $ab=4;
+
+// if (is_array ($ab))
+//  {
+//      echo "yes";
+//     }
+//      else{
+//         echo "no";
+//     }
+// $name="samer tawil";
+// echo "<br>".substr($name,1,3);
+
+// $ab=array(1,2,3);
+// $c=in_array(2,$ab);
+// echo "<br>".($c);
+
+
+// print_r  (explode (" ",$arr));
+
+// print_r(str_replace ("samer","sss",$arr))."<br>";
+
+// print_r (implode (".",$arr));
+
+// print_r (implode (" ",$arr));
+
+// var_dump($arr);
+//  "<br>";
+//  print_r($arr);
+
+// $arr= array("samer", "isam", 3 );
+
+// echo(count($arr, COUNT_RECURSIVE));
+
+// echo count($arr);
+
+// echo is_array($arr)."<br>";
+
+
+// $yes = array('this', 'is', 'an array');
+
+// echo is_array($arr) ? 'Array' : 'not an Array';
+// echo "\n";
+
+
+$string=" samer tawil ";
+
+// echo substr($string,0,4);
+
+// echo in_array ("samer",$arr)? "yes" : "no";
+
+// print_r (explode(" ",$string));
+
+// print_r (str_replace("samer","bader",$arr));
+
+// echo str_replace("samer","bader",$string);
+
+// echo implode("-",$arr);
+
+// echo strlen($string);
+
+// $arr2=["bader","eman",4];
+
+// print_r (array_merge($arr,$arr2));
+
+// echo strpos($string,"t");
+// echo strlen($string)."<br>";
+// echo strlen(trim($string," "));
+
+
+$arr2=[10,1,2,30,4,5,9,"f"];
+//   rsort($arr2)  ;
+//   print_r ($arr2);
+// $a=sort($arr2);
+// echo print_r ($a);
+// echo  in_array(100, $arr2 )? "yes" : "no" ;
+// $va1=null;
+// print_r( is_null($var1)?"yes": "no");
+
+// echo gettype($arr2);
+// $var1="4 mon";
+// // echo gettype($var1);
+// // $var1=$var1+4;
+
+
+// echo $var1+1;
+// $var1=5;
+// echo gettype($var1)."<br>";
+// $var1=(string) $var1 ;
+// echo gettype($var1);
+
+// $var1="5 hello";
+
+// // $var1=(integer)$var1;
+
+// $var2=(integer)$var1;
+//   echo $var2;
+// echo  gettype($var2);
+
+// $var1=100; echo gettype($var1)."<br>"; $var1=(string)$var1;echo $var1.gettype($var1);
+
+// for ($i=0; $i<10; $i++) {    echo "hello";   }
+
+// function sum_fun()
+// {
+//     echo "hello samer";
+// }
+
+// sum_fun();
+
+// function sum_fun2($var1=0,$var2=0 )
+//  {
+
+//     echo $var1+$var2;
+
+// }
+
+// sum_fun2(10,20);
+
+// function sum_fun3 ($var1=0,$var2=0)
+// {
+//    $var3= $var1+$var2;
+//     return $var3;
+// }
+
+// $result=sum_fun3(10,20);
+// echo $result;
+
+// function array_fun ($var1=0,$var2=0) {
+//     $arr=[];
+//     $arr[]=$var1+$var2;
+//     $arr[]=$var1-$var2;
+//     $arr[]=$var1*$var2;
+//     return $arr;
+// }
+// $result=array_fun(20,10);
+// print_r($result);
+
+
+
+// for($i=0;$i<=count($result)-1;$i++) {
+//     print_r("<br>".$result[$i]);
+// }
+
+// $arr3= ['samer',1,2,10,90,30,1000,'samer','eman'] ;
+// $arr4=sort($arr3);
+//  print_r( sort($arr3));
+
+$arr4=["name"=>"samer","salary"=>1000,"email"=>"s@s.com"];
+
+// print_r($arr4["name"]);
+
+// VAR_DUMP (array_change_key_case($arr4,CASE_LOWER));
+
+// echo count($arr4);
+
+$arr_num=[1,2,3,4,5,6];
+
+// sort($arr_num);
+// print_r ($arr_num);
+// echo "<br>";
+
+// rsort($arr_num);
+// print_r($arr_num);
+// $st1="samer tawil";
+// print_r(implode(".",$arr_num));
+
+// $var4=null;
+// $var5='';
+// echo isset($var5);
+
+
+$arr5=["samer","hesham","eman",5,4];
+
+// foreach ($arr5 as $arr) {
+//     echo $arr."<hr>";
+// }
+
+// $arr6=["1","2","3",5,4];
+// foreach($arr6 as $arr6_value) {
+//     echo $arr6_value;
+// }
+
+$arr7=["name"=>"samer","age"=>27,"email"=>"s@s.com" ];
+
+
+// foreach($arr7 as $arr_key=>$arr_value) {
+//     echo $arr_key." : ".$arr_value."<br>";
+// }
+
+// $data = [
+//     ["id" => 1, "name" => 'Tom'],
+//     ["id" => 2, "name" => 'Fred'],
+// ];
+// foreach ($data as ["id" => $id, "name" => $name]) {
+//     echo "id: $id, name: $name\n";
+// }
+// echo PHP_EOL;
+
+// foreach($arr5 as $value) {
+// echo $value;
+// }
+
+// for($i=0;$i<15;$i++) {
+//     if ($i==10) {
+//         continue;
+//     }
+//     echo $i;
+// }
+$arr8=["samer","eman",5,4,6];
+
+// for($i=0;$i<=count($arr8)-1;$i++) {
+// if ($arr8[$i]=="samer") {
+//     continue;
+// }
+// echo $arr8[$i]."<br>";
+// }
+
+// for ($i=0; $i <15 ; $i++) {
+//    if ($i==10) {
+//        break;
+//    }
+//    echo $i."<br>";
+// }
+
+
+// for($i=0;$i<=count($arr8)-1;$i++) {
+
+// if (($arr8[$i])=="eman") {
+//     continue;
+// }
+// echo $arr8[$i]."<br>";
+// echo current($arr8);
+// }
+
+// function samer_fun($var1=null) {
+//     $var2=$var1+10;
+//     return array($var2,$var1,10,12,5,"samer");
+// }
+
+// print_r(  samer_fun(5));
+
+// $name="samer";
+// $name2="eman";
+
+// function test_fun () {
+
+//     echo  $GLOBALS['name'];
+//     echo $GLOBALS['name2'];
+
+// }
+// test_fun();
+
+// phpinfo();
+
+// echo $_SERVER['QUERY_STRING'];
+// echo $_SERVER['HTTP_REFERER'];
+// echo $_SERVER['SERVER_PORT'];
+// echo $_SERVER['DOCUMENT_ROOT'];
+// echo $_SERVER['REQUEST_METHOD'];
+
+
+$value1="5 years";
+
+  echo  $value1+5  ;
